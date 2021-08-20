38e02b3ffb4bd48018ed71b60ce37e95822ad890
Onesmo Kasongo <kasongoonesmo33@gmail.com>
2021-08-20 20:24:27 +0530

    added navbar

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+document.querySelector('.menu').addEventListener('click', ()=>{
+    document.querySelectorAll('.target').forEach((item)=>{
+        item.classList.toggle('change')
+    })
+})
+
+
 const icons =  document.querySelectorAll('.section-1-icons i')
 let i = 1
 
@@ -12,3 +19,4 @@ setInterval( ()=>{
         icon.nextElementSibling.classList.add('change')
     }
 }, 4000)
+
